b36bff0fbb5091850dfe3b53b1d75964ec99549e
Kadeem Compton <52226460+KadeemComp@users.noreply.github.com>
2021-11-09 10:19:48 -0400

    Created started code
    I created a maven project.
    src App.java is where I will start coding the project.
    FilesToRename is a folder of sample pdfs i downloaded online.

---
+++ oopproject/src/main/java/comp3607/project/App.java
@@ -0,0 +1,13 @@
+package comp3607.project;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
