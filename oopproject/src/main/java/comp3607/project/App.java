eae221020d574b3ab81d5a2727da05d76e0d7276
Kadeem Compton <52226460+KadeemComp@users.noreply.github.com>
2021-11-09 10:31:13 -0400

    Read files in a folder
    Added code to read files from the sample folder and print the names of the files to the screen.

--- oopproject/src/main/java/comp3607/project/App.java
+++ oopproject/src/main/java/comp3607/project/App.java
@@ -1,5 +1,8 @@
 package comp3607.project;
 
+import java.util.*;
+import java.io.File;
+
 /**
  * Hello world!
  */
@@ -7,7 +10,20 @@ public final class App {
     private App() {
     }
 
+    public static void  listFilesForFolder(final File folder) {
+        for (final File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                listFilesForFolder(fileEntry);
+            } else {
+                System.out.println(fileEntry.getName());
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        final File folder = new File("FilesToRename");
+        listFilesForFolder(folder);
     }
 }
