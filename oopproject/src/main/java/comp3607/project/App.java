3682459a702abd059d6662232e2d1e3804645ed4
Kadeem Compton <52226460+KadeemComp@users.noreply.github.com>
2021-11-10 08:25:09 -0400

    reorganized the code base

--- oopproject/src/main/java/comp3607/project/App.java
+++ oopproject/src/main/java/comp3607/project/App.java
@@ -1,19 +1,28 @@
 package comp3607.project;
 
 import java.util.*;
+
+import org.apache.commons.io.FileUtils;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 /**
- * Hello world!
+ * Hello world
  */
+// Steps from
 public final class App {
     private App() {
     }
 
-    //https://stackoverflow.com/questions/1844688/how-to-read-all-files-in-a-folder-from-java
+    // https://stackoverflow.com/questions/1844688/how-to-read-all-files-in-a-folder-from-java
     // credit to the above link for the code
 
-    public static void  listFilesOfFolder(final File folder) {
+    public static void listFilesOfFolder(final File folder) {
         for (final File fileEntry : folder.listFiles()) {
             if (fileEntry.isDirectory()) {
                 listFilesOfFolder(fileEntry);
@@ -21,28 +30,60 @@ public final class App {
                 System.out.println(fileEntry.getName());
             }
         }
+    }// read names of assignment files [STEP 2]
+
+    public static ArrayList<File> arraylist(final File folder) {
+        ArrayList<File> files = new ArrayList<>();
+        for (final File fileEntry : folder.listFiles()) {
+            files.add(fileEntry);
+        }
+        return files;
+    } // To put the files in an array, might use this to renamed files
+
+//     public static void copyfiles(String originalFile, String destFile) {
+//         File rootFile = new File(originalFile);
+//         ArrayList<File> files = new ArrayList<>();
+//         for (final File fileEntry : rootFile.listFiles()) {
+//             files.add(fileEntry);
+//         }
+//         System.out.println(files);
+//     } my own code attempting to copy and paste the two files here but pieces missing 
+
+    public static void copyfiles(File from, File to) throws IOException {
+        FileUtils.copyFile(from, to);
     }
+    // public static void copyFile(String from, String to) throws IOException {// this function suppose the same thing as the above function
+    // Path src = Paths.get(from);
+    // Path dest = Paths.get(to);
+    // Files.copy(src.toFile(), dest.toFile());
+    // }
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
 
         final File folder = new File("FilesToRename");
-        listFilesOfFolder(folder);
 
-            //Attempting to create the nested folder in "FilesToRename". Not working right now.
-          String path = "FilesToRename";  
-          //Using Scanner class to get the folder name from the user  
-          System.out.println("Enter the name of the desired a directory: ");   
-          //Instantiate the File class   
-          File f1 = new File(path);  
-          //Creating a folder using mkdir() method  
-          boolean bool = f1.mkdir();  
-          if(bool){  
-             System.out.println("Folder is created successfully");  
-          }else{  
-             System.out.println("Error Found!");  
-          }  
+        // listFilesOfFolder(folder);
+        // ========================[ STEP 5]=============================
+        File nestedFolder = new File(folder + "/renamedFiles");
+        if (folder.exists()) {
+            // nestedFolder.createNewFile();//needed to create a file
+            nestedFolder.mkdir();// to create a folder/directory
+            System.out.println("Folder created");
+        }
+        // ===============================================================
+        for (File files : arraylist(folder)) {
+            // if (folder.exists()) {
+            // files.getAbsolutePath();
+            // // copy file in filestorename
+            // if (nestedFolder.exists()) {
+            // copyfiles(files, nestedFolder);
+
+            // }
+            // }
+
+        }
+
     }
-    
+
 }
