26f1178b7efe28ddd33a241fbd05283df97365dd
Kadeem Compton <52226460+KadeemComp@users.noreply.github.com>
2021-11-10 08:11:42 -0400

    Update App.java

--- oopproject/src/main/java/comp3607/project/App.java
+++ oopproject/src/main/java/comp3607/project/App.java
@@ -2,6 +2,7 @@ package comp3607.project;
 
 import java.util.*;
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Hello world!
@@ -10,10 +11,10 @@ public final class App {
     private App() {
     }
 
-    //https://stackoverflow.com/questions/1844688/how-to-read-all-files-in-a-folder-from-java
+    // https://stackoverflow.com/questions/1844688/how-to-read-all-files-in-a-folder-from-java
     // credit to the above link for the code
 
-    public static void  listFilesOfFolder(final File folder) {
+    public static void listFilesOfFolder(final File folder) {
         for (final File fileEntry : folder.listFiles()) {
             if (fileEntry.isDirectory()) {
                 listFilesOfFolder(fileEntry);
@@ -21,28 +22,24 @@ public final class App {
                 System.out.println(fileEntry.getName());
             }
         }
-    }
+    }// read names of assignment files [STEP 2]
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
 
         final File folder = new File("FilesToRename");
+
         listFilesOfFolder(folder);
 
-            //Attempting to create the nested folder in "FilesToRename". Not working right now.
-          String path = "FilesToRename";  
-          //Using Scanner class to get the folder name from the user  
-          System.out.println("Enter the name of the desired a directory: ");   
-          //Instantiate the File class   
-          File f1 = new File(path);  
-          //Creating a folder using mkdir() method  
-          boolean bool = f1.mkdir();  
-          if(bool){  
-             System.out.println("Folder is created successfully");  
-          }else{  
-             System.out.println("Error Found!");  
-          }  
+        // ========================[ STEP 5]=============================
+        File nestedFolder = new File(folder + "/RenamedFiles");
+        if (folder.exists()) {
+            // nestedFolder.createNewFile();//needed to create a file
+            nestedFolder.mkdir();// to create a folder/directory
+            System.out.println("File created");
+        }
+        // ===============================================================
+
     }
-    
+
 }
