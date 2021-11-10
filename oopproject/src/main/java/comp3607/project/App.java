c8b637e0181eb3b733d5bb223636bce531186fda
Kadeem Compton <52226460+KadeemComp@users.noreply.github.com>
2021-11-10 17:23:35 -0400

    Revert "Code to create nested folder"
    This reverts commit 85e54a3fd75079ca16581d9f06f8e3336337a20b.

--- oopproject/src/main/java/comp3607/project/App.java
+++ oopproject/src/main/java/comp3607/project/App.java
@@ -12,37 +12,21 @@ public final class App {
 
     //https://stackoverflow.com/questions/1844688/how-to-read-all-files-in-a-folder-from-java
     // credit to the above link for the code
-
-    public static void  listFilesOfFolder(final File folder) {
+    
+    public static void  listFilesForFolder(final File folder) {
         for (final File fileEntry : folder.listFiles()) {
             if (fileEntry.isDirectory()) {
-                listFilesOfFolder(fileEntry);
+                listFilesForFolder(fileEntry);
             } else {
                 System.out.println(fileEntry.getName());
             }
         }
     }
 
-
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
         final File folder = new File("FilesToRename");
-        listFilesOfFolder(folder);
-
-            //Attempting to create the nested folder in "FilesToRename". Not working right now.
-          String path = "FilesToRename";  
-          //Using Scanner class to get the folder name from the user  
-          System.out.println("Enter the name of the desired a directory: ");   
-          //Instantiate the File class   
-          File f1 = new File(path);  
-          //Creating a folder using mkdir() method  
-          boolean bool = f1.mkdir();  
-          if(bool){  
-             System.out.println("Folder is created successfully");  
-          }else{  
-             System.out.println("Error Found!");  
-          }  
+        listFilesForFolder(folder);
     }
-    
 }
