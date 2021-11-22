261dfa4274ef0fd7259b2dd1eb2e2c2d1b3b8139
Rebekah <rebekah.stewart@my.uwi.edu>
2021-11-22 18:37:05 -0400

    Testing
    Removed other test cases because the imports is not working all of a sudden

--- code/src/main/java/refactor/code/FileFixer.java
+++ code/src/main/java/refactor/code/FileFixer.java
@@ -1,7 +1,7 @@
 package refactor.code;
-public class FileFixer{
 
-    
+public class FileFixer {
+
     public static void main(String[] args) {
         System.out.println("Hello is this working");
 
