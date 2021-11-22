1d93b62aad45de62495f0586af15618cf9deb3c8
NicholasJK <71773960+NicholasJK@users.noreply.github.com>
2021-11-21 23:47:05 -0400

    copy and problemsubmission to do in  mediator
    Need to create an entire new project and recompile
    Refactored folder stopped working again idky... src files should be good tho

--- demo/src/main/java/refactor/app/RenameFile.java
+++ demo/src/main/java/refactor/app/RenameFile.java
@@ -13,7 +13,31 @@ public class RenameFile {
      * @param asg
      */
     public void Rename(String folderDir, AssignmentFile asg) {
-        // TODO implement here
+        String renamedFileName = new String();
     }
+
+    public void renameFile(Student student){
+        String renamedFileName = new String();
+        String originalFileName = student.studentSubmission.getFileName;
+
+        for(int i = 0; i<student.studentNames.length; i++){
+            renamedFileName += studentNames[i];
+        }
+        renamedFileName = renamedFileName.trim();
+        renamedFileName += "_"+student.getParticipantID+"_";
+        renamedFileName += "assignsubmission_file_";
+        renamedFileName += originalFileName;
+
+        if(originalFileName.endsWith(".pdf")){
+            renamedFileName+=".pdf";
+        }
+        
+        if(originalFileName.endsWith(".docx")){
+            renamedFileName+=".docx";
+        }
+
+    }
+
+
 
 }
